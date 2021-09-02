190dfbed068ff49982fe722ba568f0800b8042db
Michael Towson <MTowson@gmail.com>
2021-09-02 14:21:24 -0700

    add ExpenseItem.Js and ExpenseItem.css

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        Date
+        <div>
+          <h2>Title</h2>
+          <div>Amount</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
