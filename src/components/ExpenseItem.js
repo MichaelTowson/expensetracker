2059b05a813b56a93f3f0bea2feaab21c46fc33d
Michael Towson <MTowson@gmail.com>
2021-09-02 16:30:56 -0700

    add react import to files for clarity

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
-import ExpenseDate from './ExpenseDate'
-import Card from './Card'
+import React from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
-
 function ExpenseItem(props) {
-
   return (
     <div>
       <Card className="expense-item">
-			<ExpenseDate date={props.date}/>
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
