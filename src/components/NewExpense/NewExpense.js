f321c53262009574ba655168469d50fafec1649a
Michael Towson <MTowson@gmail.com>
2021-09-04 17:47:27 -0700

    add NewExpense.js, NewExpenseForm.js, and their css

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewExpense.css';
+
+const NewExpnse = () => {
+  return (
+    <div className="new-expense">
+      <form></form>
+    </div>
+  )
+};
+
+export default NewExpense;
