3a7fd129ad4488917802a6a8ba8950115544fdb1
Michael Towson <MTowson@gmail.com>
2021-09-09 14:09:30 -0700

    add functionality to form to add expense by clicking button and cancel

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewExpenseForm from './NewExpenseForm'
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData, //Get all the information that was passed in, and then add ID
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData)
+    setIsEditing(false);
+  }
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   }
 
   return (
     <div className="new-expense">
-      <NewExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {/* If not in the state of adding an expense" */}
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+
+      {/* If in the state of adding an expense" */}
+      {isEditing && (
+        <NewExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />
+      )}
     </div>
   );
 };
